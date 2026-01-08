2429893975ab2242767c2f7ef586ec18cf7a69a5
Debankov <81907141+Debankov@users.noreply.github.com>
2026-01-08 05:56:26 -0800

    Add alternative solution for even digit count
    Added an alternative solution for counting numbers with even digits.

--- 1295. Find Numbers with Even Number of Digits.py
+++ 1295. Find Numbers with Even Number of Digits.py
@@ -7,4 +7,14 @@ class Solution:
                 count += 1
         return count
                 
-        
+
+# another solution
+
+#this solution just look pretty than first
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            if len(str(n)) % 2 == 0:
+                res+= 1
+        return res

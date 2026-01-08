736043440454fff35d9ec91a6fa830b84f6cf5cc
Debankov <81907141+Debankov@users.noreply.github.com>
2026-01-08 06:06:55 -0800

    Introduce another solution for sorted squares
    Added an alternative solution for sorting squares.

--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -5,4 +5,11 @@ class Solution:
         for i in sorted(nums):
             sqrNums.append(i ** 2)
         return sqrNums
-        
+
+# another solution
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(0,len(nums)):
+            nums[i] = nums[i]**2
+        return sorted(nums)

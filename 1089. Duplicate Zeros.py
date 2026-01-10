26d7cb66d09c255dbcd3b89e2060ddaa5932fe9a
Debankov <81907141+Debankov@users.noreply.github.com>
2026-01-10 03:39:57 -0800

    Update 1089. Duplicate Zeros.py

--- 1089. Duplicate Zeros.py
+++ 1089. Duplicate Zeros.py
@@ -12,3 +12,18 @@ class Solution:
                 i += 2
             else:
                 i += 1
+
+# another way to solution 
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        tempArray = list()
+        for i,val in enumerate(arr):
+            tempArray.append(val)
+            if val == 0:
+                tempArray.append(0)
+                arr.pop()
+                if i == len(arr):
+                    tempArray.pop()
+        arr.clear()
+        arr.extend(tempArray)
